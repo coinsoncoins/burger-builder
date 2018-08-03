060e67de49e21c610f708a5436e711404faf4530
Coins On Coins <coinsoncoins00@gmail.com>
2018-08-03 12:53:33 -0500

    make the modal hidden at first

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,12 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
+  }
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true })
   }
 
   updatePurchaseState = (ingredients) => {
@@ -73,13 +78,14 @@ class BurgerBuilder extends Component {
     }
     return (
       <React.Fragment>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls 
           ingredientAdded={this.addIngredientHander} 
           ingredientRemoved={this.removeIngredientHandler}
+          ordered={this.purchaseHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
           purchasable={this.state.purchasable}/>
