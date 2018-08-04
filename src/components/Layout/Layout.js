c560b07fe032013965a65be92f6a344727690ded
Jake Parks <jake@Jakes-MBP.attlocal.net>
2018-08-04 18:17:01 -0500

    add SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-export default layout;
+  sideDrawerClosedHander = () => {
+    this.setState({showSideDrawer: false});
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHander} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
