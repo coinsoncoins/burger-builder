ecf721d3093ce5938ef38d2d2303b080265031f4
Jake Parks <jake@Jakes-MBP.attlocal.net>
2018-08-04 18:33:10 -0500

    add DrawerToggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,16 +5,25 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerClosedHander = () => {
     this.setState({showSideDrawer: false});
   }
+
+  sideDrawerToggleHandler = () => {
+    this.setState( (prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      }
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHander} />
         <main className={classes.Content}>
           {this.props.children}
