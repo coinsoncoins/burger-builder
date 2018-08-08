5851f2c085e3848770e0b957ca0904932abbda16
Jake Parks <jake@Jakes-MBP.attlocal.net>
2018-08-08 10:07:41 -0500

    validation data being set

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Your Name'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
         },
         street: {
             elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Street'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
         },
         zipCode: {
             elementType: 'input',
@@ -31,7 +39,13 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'ZIP Code'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true,
+              minLength: 5,
+              maxLength: 5
+            },
+            valid: false
         },
         country: {
             elementType: 'input',
@@ -39,7 +53,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Country'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
         },
         email: {
             elementType: 'input',
@@ -47,7 +65,11 @@ class ContactData extends Component {
                 type: 'email',
                 placeholder: 'Your E-Mail'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
         },
         deliveryMethod: {
             elementType: 'select',
@@ -61,7 +83,28 @@ class ContactData extends Component {
         }
     },
     loading: false
-}
+  }
+
+  checkValidity = (value, rules) => {
+    if (!rules) {
+      return true;
+    }
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.trim().length >= rules.minLength && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.trim().length <= rules.minLength && isValid;
+    }
+
+    return isValid;
+  }
 
   orderHandler = (event) => {
     console.log("[ContactData] orderHandler");
@@ -94,8 +137,10 @@ class ContactData extends Component {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputName] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputName] = updatedFormElement;
     this.setState({orderForm: updatedOrderForm});
+    console.log(updatedFormElement);
   }
   
   render() {
