8c08e7e41d03fbf56836771ce940ccb9cbd99530
Jake Parks <jake@Jakes-MacBook-Pro.local>
2018-08-07 21:39:26 -0500

    Orders page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import classes from "./Orders.css";
+import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get("/orders.json")
+      .then((response) => {
+        const fetchedOrders = [];
+        for(let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({orders: fetchedOrders, loading: false})
+      }).catch((error) => {
+        this.setState({loading: false});
+        console.log(error);
+      })
+  }
+  render() {
+    let orders = this.state.orders.map((order, index) => {
+      return <Order 
+        key={order.id} 
+        ingredients={order.ingredients}
+        price={+order.price}
+      />
+    })
+    return (
+      <div>
+        {orders}
+      </div>
+    );
+  }
+}
+
+export default withErrorHandler(Orders, axios);
