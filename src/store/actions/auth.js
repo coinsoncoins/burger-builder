1a999d3ac905a76d1e31f9617bc76ae8ae35bad4
Jake Parks <jake@Jakes-MBP.attlocal.net>
2018-08-11 10:05:24 -0500

    store token in localStorage so can login on page refresh

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,6 +30,9 @@ export const setAuthRedirectPath = (path) => {
 }
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -60,11 +63,33 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response)
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       }).catch(error => {
         console.log(error);
         dispatch(authFail(error.response.data.error));
       })
+  }
+}
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date.getSeconds()));
+      }
+    }
   }
 }
